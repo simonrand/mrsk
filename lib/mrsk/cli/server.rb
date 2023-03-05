581d2509c72ebfb641c1e4a9088407d546de5678
Simon Rand <simonrand@gmail.com>
2023-03-05 10:56:51 +0000

    WIP Ensure curl is installed

--- lib/mrsk/cli/server.rb
+++ lib/mrsk/cli/server.rb
@@ -1,6 +1,10 @@
 class Mrsk::Cli::Server < Mrsk::Cli::Base
   desc "bootstrap", "Ensure Docker is installed on servers"
   def bootstrap
-    on(MRSK.hosts + MRSK.accessory_hosts) { execute "which docker || (apt-get update -y && apt-get install docker.io -y)" }
+    on(MRSK.hosts + MRSK.accessory_hosts) do
+      # TODO: Should only run update once
+      execute "which curl || (apt-get update -y && apt-get install curl -y)"
+      execute "which docker || (apt-get update -y && apt-get install docker.io -y)"
+    end
   end
 end
